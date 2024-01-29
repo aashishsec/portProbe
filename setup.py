39e8981d13d88073f93de22749c46f15fcc9a332
AashishSec ఆశిష్ <65489287+aashishsec@users.noreply.github.com>
2024-01-29 09:03:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,23 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='portProbe',
+    version='1.0.2',
+    description='portProbe is a tool designed to efficiently probe for open ports. It will take both IP Address and Subdomains.',
+    author='AashishSec',
+    url='https://github.com/aashishsec/httpAlive',
+    packages=find_packages(),
+    install_requires=[
+        'sockets',
+        'ipaddress',
+        'colorama',
+    ],
+    extras_require={
+        'dev': ['argparse', 'concurrent.futures','random'],
+    },
+    entry_points={
+        'console_scripts': [
+            'portProbe = portProbe.portProbe:main',
+        ],
+    },
+)
